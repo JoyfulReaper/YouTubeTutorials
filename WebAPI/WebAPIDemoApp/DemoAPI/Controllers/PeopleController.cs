0c875ee8e46758e4f3a8132e7af47e5d26d6233d
Kyle Givler <xvegan88x@gmail.com>
2021-01-18 18:04:58 -0500

    WebAPIDemo

---
+++ WebAPI/WebAPIDemoApp/DemoAPI/Controllers/PeopleController.cs
@@ -0,0 +1,74 @@
+﻿using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PeopleController : ControllerBase
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Kyle", LastName = "Givler", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", Id = 2 });
+            people.Add(new Person { FirstName = "Bilbo", LastName = "Baggins", Id = 3 });
+        }
+
+        [Route("GetFirstNames")]
+        [HttpGet]
+        public List<string> GetFirstNames()
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+
+            return output;
+        }
+
+        // GET: api/<PeopleController>
+        [HttpGet]
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET api/<PeopleController>/5
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<PeopleController>
+        [HttpPost]
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+
+        // PUT api/<PeopleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PeopleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            people.Remove(people.Where(x => x.Id == id).FirstOrDefault());
+        }
+    }
+}
