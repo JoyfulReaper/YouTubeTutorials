7d4297275db6f2e1e48c9131c9338c91f6316b2b
Kyle Givler <xvegan88x@gmail.com>
2021-02-24 18:25:58 -0500

    https://www.youtube.com/watch?v=LSkbnpjCEkk

---
+++ ExceptionsDemo/ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExceptionsLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoCode demo = new DemoCode();
+
+            try
+            {
+                int result = demo.GrandparentMethod(4);
+                Console.WriteLine($"The value at the given postion is { result }");
+            }
+            catch (ArgumentException ex)
+            {
+                // Multiple catches are okay!
+                // This is the 1st catch block that matches. Order matters.
+                Console.WriteLine("You gave us bad information. Bad user!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                var inner = ex.InnerException;
+
+                while(inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException; // Full stack trace 
+                }
+            }
+        }
+    }
+}
---
+++ ExceptionsDemo/ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandparentMethod(int postion)
+        {
+            int output = 0;
+
+            Console.WriteLine("\"Open\" Database Connection");
+
+            try
+            {
+                output = ParentMethod(postion);
+            }
+            catch (Exception ex)
+            {
+                // Could log the exception here
+                //throw; // Pass exception "up the chain" like not catching. Re-trhow after running the finally code!
+                throw new ArgumentException("You passed in bad data", ex);
+            }
+            finally
+            {
+                // Run this code anyway before passing the exception up the chain
+                Console.WriteLine("\"Close\" Database Connection");
+            }
+
+            return output;
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+
+            //try
+            //{
+                int[] numbers = new int[] { 1, 4, 7, 2 };
+                output = numbers[position];
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+            return output;
+        }
+    }
+}
