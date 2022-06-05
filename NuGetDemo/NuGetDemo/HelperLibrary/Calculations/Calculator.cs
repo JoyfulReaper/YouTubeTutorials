21a99b08bfcc745563df1295377e74ec9a713092
Kyle Givler <xvegan88x@gmail.com>
2022-06-05 12:20:13 -0400

    NuGet Demo

---
+++ NuGetDemo/NuGetDemo/HelperLibrary/Calculations/Calculator.cs
@@ -0,0 +1,26 @@
+﻿// https://www.youtube.com/watch?v=AF1y9gLcxjQ
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IAmTimCoery.Helpers.Calculations
+{
+    public class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
