a1e19ba7e3a048d62dc9ae81f15efee0ccbdceeb
Kyle Givler <xvegan88x@gmail.com>
2022-06-05 12:51:37 -0400

    Basic gRPC demo
    https://www.youtube.com/watch?v=QyxCX2GYHxk

---
+++ GrcpDemo/GrcpClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using GrcpServer;
+using Grpc.Net.Client;
+
+var input = new HelloRequest
+{
+    Name = "Tim"
+};
+
+var channel = GrpcChannel.ForAddress("https://localhost:7099");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine(reply.Message);
+
+Console.ReadKey();
