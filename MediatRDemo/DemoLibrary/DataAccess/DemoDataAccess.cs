68a2a1fa10f10a7c347cf347251d78a0ffee43e7
Kyle Givler <xvegan88x@gmail.com>
2022-11-19 12:46:56 -0500

    Mediator + CRQS demo
    https://www.youtube.com/watch?v=yozD5Tnd8nw

---
+++ MediatRDemo/DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess;
+public class DemoDataAccess : IDataAccess
+{
+    public DemoDataAccess()
+    {
+        people.Add(new PersonModel { Id = 1, FirstName = "Tim", LastName = "Corey" });
+        people.Add(new PersonModel { Id = 2, FirstName = "Sue", LastName = "Storm" });
+    }
+
+    private List<PersonModel> people = new();
+
+    public List<PersonModel> GetPeople()
+    {
+        return people;
+    }
+
+    public PersonModel InsertPerson(string firstName, string lastName)
+    {
+        PersonModel person = new PersonModel { LastName = lastName, FirstName = firstName };
+        person.Id = people.Max(p => p.Id) + 1;
+        people.Add(person);
+
+        return person;
+    }
+}
