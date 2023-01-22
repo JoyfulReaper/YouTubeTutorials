bd722b122b7703dafb79515b0dab65cc44de20f9
Kyle Givler <xvegan88x@gmail.com>
2023-01-22 16:57:55 -0500

    https://www.youtube.com/watch?v=kOnMMKxwm3I
    Blazor Auth - Part 2

--- BlazorAuthenticationTutorial/BlazorAuthenticationTutorial/Client/Program.cs
+++ BlazorAuthenticationTutorial/BlazorAuthenticationTutorial/Client/Program.cs
@@ -1,7 +1,9 @@
 // https://www.youtube.com/watch?v=Yh16E2u2pio
+// https://www.youtube.com/watch?v=kOnMMKxwm3I
 
 global using Microsoft.AspNetCore.Components.Authorization;
 using BlazorAuthenticationTutorial.Client;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,5 +14,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStoreProvider>();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
