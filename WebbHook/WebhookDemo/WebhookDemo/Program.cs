d668bd2c694befea33f6d38d17c63af799d29fa3
Kyle Givler <xvegan88x@gmail.com>
2023-09-02 13:18:36 -0400

    add api key

--- WebbHook/WebhookDemo/WebhookDemo/Program.cs
+++ WebbHook/WebhookDemo/WebhookDemo/Program.cs
@@ -2,6 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapPost("/webhook", async context => {
+    if (!context.Request.Headers.ContainsKey("Authorization"))
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return;
+    }
+
+    var apiKey = context.Request.Headers["Authorization"];
+    if(apiKey != "APIKEY")
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return;
+    }
+
     var requestBody = await context.Request.ReadFromJsonAsync<WebhookPayload>();
     await Console.Out.WriteLineAsync($"Header: {requestBody.header}, Body: {requestBody.body}");
     context.Response.StatusCode = StatusCodes.Status200OK;
