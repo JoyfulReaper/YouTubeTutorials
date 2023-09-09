30a0c8f38e9334ba81e0078773ec4fe019d145b9
Kyle Givler <xvegan88x@gmail.com>
2023-09-09 09:56:29 -0400

    computed property

--- Vue3/app.js
+++ Vue3/app.js
@@ -33,6 +33,14 @@ const app = Vue.createApp({
         handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
