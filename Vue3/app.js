1e230856024115f92870c9fe24fc037e675b4a27
Kyle Givler <xvegan88x@gmail.com>
2023-09-09 08:54:54 -0400

    https://www.youtube.com/watch?v=YrxBCBibVo0&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=2

---
+++ Vue3/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    // Data, functions
+    // template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+})
+
+app.mount('#app')
