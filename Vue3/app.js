734de407b33659ba48354ab3cd73612f3cb58e44
Kyle Givler <xvegan88x@gmail.com>
2023-09-09 09:32:11 -0400

    Vue Demo

--- Vue3/app.js
+++ Vue3/app.js
@@ -3,10 +3,18 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>'
     data() {
         return {
+            url: 'https://kgivler.com',
             showBooks: true,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true },
+            ],
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
         }
     },
     methods: {
@@ -15,6 +23,16 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
