60555b8512949c0e821cf0eef2239b3cbf67175a
Kyle Givler <xvegan88x@gmail.com>
2023-09-10 12:19:15 -0400

    Modal Challange

--- Vue3/modal-project/src/App.vue
+++ Vue3/modal-project/src/App.vue
@@ -3,9 +3,27 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">Delete the internet</a>
+        <a href="#">Do something</a>
+      </template>
+      <h1>Awesome Title</h1>
+      <p>Awesome text of the modal</p>
+    </Modal>
   </div>
   <button @click.alt="toggleModal">Open Modal (alt)</button>
+  <button @click="toggleModal2">Open Modal2</button>
   <input type="text" ref="name">
   <button @click="handleClick">Click me</button>
 </template>
@@ -24,7 +42,8 @@ export default {
       title: 'My first Vue App :)',
       header: 'Sign up for Giveaway!',
       text: 'Grab your ninja swag for half price!',
-      showModal: false
+      showModal: false,
+      showModal2: false
     }
   },
   methods: {
@@ -35,6 +54,10 @@ export default {
     },
     toggleModal () {
       this.showModal = !this.showModal
+    },
+    toggleModal2()
+    {
+      this.showModal2 = !this.showModal2
     }
   }
 }
