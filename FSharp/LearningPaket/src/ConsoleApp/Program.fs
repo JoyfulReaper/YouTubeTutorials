4e4d004983bffd23de42a600b54bc60f8d4b04a6
Kyle Givler <xvegan88x@gmail.com>
2023-09-23 21:28:27 -0400

    Paket

---
+++ FSharp/LearningPaket/src/ConsoleApp/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open FSharpPlus
+
+
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#"
+    let message = String.replace "from" "to" message
+    printfn "Hello world %s" message
+    0
