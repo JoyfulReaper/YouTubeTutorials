d7dd2d8a3349921411f2ca79746acbb6ff6f593b
Kyle Givler <xvegan88x@gmail.com>
2023-09-24 14:47:11 -0400

    Vue: Form handling

--- Vue3/reaction-timer/src/App.vue
+++ Vue3/reaction-timer/src/App.vue
@@ -2,15 +2,18 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults">Reaction Time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
 export default {
   name: 'App',
   components: {
-    Block
+    Block,
+    Results
   },
   methods: {
     start() {
@@ -43,5 +46,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button:disabled {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
