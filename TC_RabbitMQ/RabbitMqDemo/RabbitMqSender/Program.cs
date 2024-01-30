ad3b9ea84c3b12f56b2bd51a25baca53b481da60
Kyle Givler <xvegan88x@gmail.com>
2024-01-30 14:25:12 -0500

    TC RabbitMQ

---
+++ TC_RabbitMQ/RabbitMqDemo/RabbitMqSender/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExachange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+// byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello YouTube Again");
+// channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Sending message {i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message #{i}");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+cnn.Close();
